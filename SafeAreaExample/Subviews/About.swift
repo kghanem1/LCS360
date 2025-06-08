9e198fb1d3a7d5c4aba784970c7d05462c892336
Khalid Ghanem <kghanem@lcs.on.ca>
2025-06-08 17:39:33 -0400

    Made lots of changes and updates to the map pages, created some new buttons for example when u press a map it brings a fullscreen version of it.

---
+++ SafeAreaExample/Subviews/About.swift
@@ -0,0 +1,18 @@
+//
+//  About.swift
+//  SafeAreaExample
+//
+//  Created by Khalid Ghanem on 2025-06-06.
+//
+
+import SwiftUI
+
+struct About: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    About()
+}
